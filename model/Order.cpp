45cf86a12135921d1a1dbb09ee7eee5b2eaa8378
Hien <1511062@hcmut.edu.vn>
2016-06-10 17:01:06 +0700

    [ADD] class Order

---
+++ model/Order.cpp
@@ -0,0 +1,6 @@
+//
+// Created by dell on 6/10/2016.
+//
+
+#include
+ "Order.hpp"
---
+++ model/Order.hpp
@@ -0,0 +1,17 @@
+//
+// Created by dell on 6/10/2016.
+//
+
+#ifndef
+ CPPASSIGNMENT_ORDER_HPP
+#define
+ CPPASSIGNMENT_ORDER_HPP
+
+
+class Order {
+
+};
+
+
+#endif
+ //CPPASSIGNMENT_ORDER_HPP
